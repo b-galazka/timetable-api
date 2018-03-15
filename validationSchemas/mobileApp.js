3606472a059bd73f429ebec8b519102d420a3ffc
b-galazka <bartlomiejgalazka@zoho.eu>
2018-03-15 16:54:40 +0100

    improve data validation

--- validationSchemas/mobileApp.js
+++ validationSchemas/mobileApp.js
@@ -1,22 +1,9 @@
 const Joi = require('joi');
 
-const ipOctet = '((25[0-5])|(2[0-4][0-9])|(1[0-9]{2})|([1-9]?[0-9]))';
-const ip = `((${ipOctet}\\.){3}${ipOctet})`;
-const port = '(:\\d+)';
-const urlVariable = '([\\w]+=[\\w-\\.%]+)';
-const urlQuery = `(\\?${urlVariable}(&${urlVariable})*)`;
-const protocol = 'https?://';
-const domain = '(([\\da-z]+(-[\\da-z]+)*\\.)+[a-z]{2,6})';
-const urlPath = '(/([\\w-\.%]+/)*[\\w-\.%]+)';
-const urlHash = '(#\\w+)';
-
-const apkFileUrl = `^${protocol}(${ip}|${domain})${port}?(/|${urlPath})?/?${urlQuery}?/?${urlHash}?$`;
-const apkFileUrlRegex = new RegExp(apkFileUrl);
-
 module.exports = Joi.object().keys({
 
     version: Joi.string()
-        .regex(/^\d{1,3}\.\d{1,2}$/),
+        .regex(/^\d{1,3}\.\d{1,2}$/, 'valid version format'),
 
     changelog: Joi.array()
         .items(Joi.string()),
@@ -26,5 +13,5 @@ module.exports = Joi.object().keys({
 
     apkFileUrl: Joi.string()
         .allow('')
-        .regex(apkFileUrlRegex, 'valid http(s) URL with domain name or IP')
+        .uri()
 }).or('version', 'changelog', 'message', 'apkFileUrl');

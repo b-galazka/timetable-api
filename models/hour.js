d5691ee793e327992dace64602d5c2a4dd6ab8fd
b-galazka <bartlomiejgalazka@zoho.eu>
2018-03-21 12:59:50 +0100

    improve hours sorting

--- models/hour.js
+++ models/hour.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const hourSchema = new mongoose.Schema(
+    
     {
         start: {
             type: String,
@@ -20,16 +21,31 @@ const hourSchema = new mongoose.Schema(
 
 hourSchema.statics = {
 
-    loadList() {
+    async loadList() {
 
-        const ordering = {
+        const hours = await this.find();
 
-            sort: {
-                _id: 1
+        return this._sortHoursAsc(hours);
+    },
+
+    _sortHoursAsc(hours) {
+
+        return hours.sort((lessonHour1, lessonHour2) => {
+
+            const [hour1, hour1Minutes] = lessonHour1.start.split(':');
+            const [hour2, hour2Minutes] = lessonHour2.start.split(':');
+
+            const hoursDiff = hour1 - hour2;
+
+            if (hoursDiff !== 0) {
+
+                return hoursDiff;
             }
-        };
 
-        return this.find({}, {}, ordering);
+            const minutesDiff = hour1Minutes - hour2Minutes;
+
+            return minutesDiff;
+        });
     }
 };
 

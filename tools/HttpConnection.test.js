0c77b8dc8d382f9bef3ed5b51025ca8b44e8840b
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-03 14:47:42 +0200

    add HttpConnection tests

---
+++ tools/HttpConnection.test.js
@@ -0,0 +1,52 @@
+const HttpConnection = require('./HttpConnection');
+
+jest.mock('http', () => require('../mocks/http'));
+
+describe('HttpConnection.get', () => {
+
+    it('should return a promise', () => {
+
+        expect(HttpConnection.get('url')).toBeInstanceOf(Promise);
+    });
+
+    it('should resolve a promise with data', async () => {
+
+        expect.assertions(1);
+
+        const data = await HttpConnection.get('url');
+
+        expect(data).toBe('mocked response from server, GET request');
+    });
+
+    it('should reject a promise with an error ' +
+        'if internal server error occured', async () => {
+
+        expect.assertions(2);
+
+        try {
+
+            await HttpConnection.get('error');
+
+        } catch (err) {
+
+            expect(err).toBeInstanceOf(Error);
+            expect(err.message).toBe('internal server error');
+        }
+    });
+
+    it('should reject a promise if res.statusCode >= 400', async () => {
+
+        expect.assertions(1);
+
+        const url = 'status error';
+
+        try {
+
+            await HttpConnection.get(url);
+
+        } catch (err) {
+
+            expect(err).toEqual({ method: 'GET', statusCode: 400, url });
+        }
+    });
+});

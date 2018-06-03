c8514ca9838e4a04fd47f1e965c3062ad8ba7a76
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-03 22:12:51 +0200

    add mongoose model mock

---
+++ mocks/mongooseModel.js
@@ -0,0 +1,45 @@
+module.exports = function getMongooseModel() {
+
+    return {
+        
+        insertMany(data) {
+
+            return new Promise((resolve, reject) => {
+
+                setImmediate(() => {
+
+                    const { collection } = this;
+
+                    if (!Array.isArray(data)) {
+
+                        return reject();
+                    }
+
+                    if (!Array.isArray(collection.data)) {
+
+                        collection.data = [];
+                    }
+
+                    this.collection.data.push(...data);
+
+                    resolve(data);
+                });
+            });
+        },
+
+        collection: {
+
+            data: null,
+
+            drop(callback) {
+
+                setImmediate(() => {
+
+                    this.data = null;
+
+                    callback();
+                });
+            }
+        }
+    };
+}

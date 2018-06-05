251ce71749e2ccbbe6157bf4b4cf75bf9d603de8
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-05 14:38:40 +0200

    add MobileApp model tests

---
+++ models/MobileApp.test.js
@@ -0,0 +1,85 @@
+const MobileApp = require('./MobileApp');
+
+describe('MobileApp.createOrUpdate', () => {
+
+    let existingApp;
+
+    const originalFindOneMethod = MobileApp.findOne;
+    const originalCreateMethod = MobileApp.create;
+
+    beforeAll(() => {
+
+        MobileApp.findOne = () => Promise.resolve(null);
+        MobileApp.create = args => Promise.resolve(args);
+    });
+
+    it('should return a promise', () => {
+
+        expect(MobileApp.createOrUpdate({})).toBeInstanceOf(Promise);
+    });
+
+    it('should create new app if it has not been created yet', async () => {
+
+        expect.assertions(2);
+
+        const spy = jest.spyOn(MobileApp, 'create');
+
+        await MobileApp.createOrUpdate({ version: '1.0' });
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith({ version: '1.0' });
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+
+    it('should resolve a promise with created app', async () => {
+
+        expect.assertions(1);
+
+        const result = await MobileApp.createOrUpdate({ version: '1.0' });
+
+        expect(result).toEqual({ version: '1.0' });
+    });
+
+    it('should update app with given data if it has been already created', async () => {
+
+        expect.assertions(2);
+
+        const save = () => {};
+        const existingApp = { a: 1, b: 2, c: 3, save };
+
+        MobileApp.findOne = () => Promise.resolve(existingApp);
+
+        const spy = jest.spyOn(existingApp, 'save');
+
+        await MobileApp.createOrUpdate({ b: 20, c: 30 });
+
+        expect(spy).toHaveBeenCalled();
+
+        spy.mockReset();
+        spy.mockRestore();
+
+        expect(existingApp).toEqual({ a: 1, b: 20, c: 30, save });
+    });
+
+    it('should resolve a promise with created app', async () => {
+
+        expect.assertions(1);
+
+        const save = function () { return this; };
+        const existingApp = { a: 1, b: 2, c: 3, save };
+
+        MobileApp.findOne = () => Promise.resolve(existingApp);
+
+        const result = await MobileApp.createOrUpdate({ b: 20, c: 30 });
+
+        expect(result).toEqual({ a: 1, b: 20, c: 30, save });
+    });
+
+    afterAll(() => {
+
+        MobileApp.findOne = originalFindMethod;
+        MobileApp.create = originalCreateMethod;
+    });
+});

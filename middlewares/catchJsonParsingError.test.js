8c94ad2ec68a6a3fc5f74f15108ead8497c2723b
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-08 12:35:01 +0200

    add catchJsonParsingError middleware tests

---
+++ middlewares/catchJsonParsingError.test.js
@@ -0,0 +1,44 @@
+const catchJsonParsingError = require('./catchJsonParsingError');
+const ExpressRequest = require('../mocks/ExpressRequest');
+const ExpressResponse = require('../mocks/ExpressResponse');
+
+describe('catchJsonParsingError middleware', () => {
+
+    let req;
+    let res;
+
+    beforeEach(() => {
+
+        req = new ExpressRequest();
+        res = new ExpressResponse();
+    });
+
+    it('should respond with status 400', () => {
+
+        const spy = jest.spyOn(res, 'status');
+
+        catchJsonParsingError(new Error(), req, res, jest.fn());
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith(400);
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+
+    it('should respond with "invalid JSON format" JSON message', () => {
+
+        const spy = jest.spyOn(res, 'send');
+
+        catchJsonParsingError(new Error(), req, res, jest.fn());
+
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        expect(spy).toHaveBeenCalledWith({
+            message: 'invalid JSON format'
+        });
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+});

8ac6e949b8623fd53dafc06123391df3b0a25b29
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-10 12:51:47 +0200

    add notFound controller tests

---
+++ controllers/notFound.test.js
@@ -0,0 +1,47 @@
+const { respondWithNotFoundMessage } = require('./notFound');
+const ExpressRequest = require('../mocks/ExpressRequest');
+const ExpressResponse = require('../mocks/ExpressResponse');
+
+describe('notFound.respondWithNotFoundMessage controller', () => {
+
+    let req;
+    let res;
+
+    beforeEach(() => {
+
+        req = new ExpressRequest();
+        res = new ExpressResponse();
+    });
+
+    it('should respond with status 404 ', () => {
+
+        expect.assertions(1);
+
+        const spy = jest.spyOn(res, 'status');
+
+        respondWithNotFoundMessage(req, res);
+
+        expect(spy).toHaveBeenCalledWith(404);
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+
+    it('should respond with "not found" JSON message ', () => {
+
+        expect.assertions(2);
+
+        const spy = jest.spyOn(res, 'send');
+
+        respondWithNotFoundMessage(req, res);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        expect(spy).toHaveBeenCalledWith({
+            message: 'not found'
+        });
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+});

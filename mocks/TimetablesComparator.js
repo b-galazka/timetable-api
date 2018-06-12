7967c4215781af45164b99d9d51f54d02f90255b
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-12 13:56:18 +0200

    add TimetablesComparator mock

---
+++ mocks/TimetablesComparator.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+
+class TimetablesComparator {
+
+    constructor(newTeachersTimetable) {
+
+        this._newTeachersTimetable = newTeachersTimetable;
+    }
+
+    areChangesInTimetable() {
+
+        if (_.isEqual(this._newTeachersTimetable, [{}])) {
+
+            return Promise.resolve(true);
+
+        } else {
+
+            throw new Error('Invalid timetable provided to Timetables Comparator');
+        }
+    }
+}
+
+module.exports = TimetablesComparator;

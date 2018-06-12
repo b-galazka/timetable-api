359ef54d6ce6ec1ed0aeeb3e05ded20f519eba07
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-12 13:55:43 +0200

    add TimetableScraper mock

---
+++ mocks/TimetableScraper.js
@@ -0,0 +1,55 @@
+const _ = require('lodash');
+
+const { scraperSelectors } = require('../config');
+
+class TimetableScraper {
+
+    constructor({ documents, selectors }) {
+
+        this.documents = documents;
+        this.selectors = selectors;
+
+        this.teachers = [];
+        this.classrooms = [];
+        this.schoolClasses = [];
+        this.hours = [];
+    }
+
+    scrap() {
+
+        const areDocumentsValid = _.isEqual(
+            this.documents,
+            ['document1', 'document2']
+        );
+
+        const areSelectorsValid = _.isEqual(this.selectors, scraperSelectors);
+
+        if (!areDocumentsValid || !areSelectorsValid) {
+
+            console.error('Invalid TimetableScraper config has been provided');
+
+            return;
+        }
+
+        this.teachers = [{}];
+        this.classrooms = [{}];
+        this.schoolClasses = [{}];
+        this.hours = [{}];
+
+        return Promise.resolve(this.getScrappedTimetable());
+    }
+
+    getScrappedTimetable() {
+
+        const { teachers, classrooms, schoolClasses, hours } = this;
+
+        return {
+            teachers,
+            classrooms,
+            schoolClasses,
+            hours
+        };
+    }
+}
+
+module.exports = TimetableScraper;

08453969ada4d646b6db6cab290177132493ba17
b-galazka <bartlomiejgalazka@zoho.eu>
2018-06-12 13:55:18 +0200

    add DocumentsDownloader mock

---
+++ mocks/DocumentsDownloader.js
@@ -0,0 +1,30 @@
+const {
+    documentsDownloaderUrls,
+    documentsDownloaderListSelector
+} = require('../config');
+
+class DocumentsDownloader {
+
+    constructor({ urls, listSelector }) {
+
+        this.baseUrl = urls.base;
+        this.listUrl = urls.list;
+        this.listSelector = listSelector;
+    }
+
+    download() {
+
+        if (
+            this.baseUrl === documentsDownloaderUrls.base &&
+            this.listUrl === documentsDownloaderUrls.list &&
+            this.listSelector === documentsDownloaderListSelector
+        ) {
+
+            return Promise.resolve([ 'document1', 'document2' ]);
+        }
+
+        console.error('Invalid DocumentsDownloader config has been provided');
+    }
+}
+
+module.exports = DocumentsDownloader;

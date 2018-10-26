4323c145fc1c38a8ce8f9683c4bcce712566c926
b-galazka <bartlomiejgalazka@zoho.eu>
2018-10-26 20:48:12 +0200

    add handling invalid HTTP method tests

---
+++ middlewares/handleInvalidHttpMethod.test.js
@@ -0,0 +1,62 @@
+const handleInvalidHttpMethod = require('./handleInvalidHttpMethod');
+const ExpressRequest = require('../mocks/ExpressRequest');
+const ExpressResponse = require('../mocks/ExpressResponse');
+
+describe('catchJsonParsingError middleware', () => {
+
+    let req;
+    let res;
+
+    beforeEach(() => {
+
+        req = new ExpressRequest();
+        res = new ExpressResponse();
+    });
+
+    it('should respond with status 405', () => {
+
+        const spy = jest.spyOn(res, 'status');
+
+        handleInvalidHttpMethod([])(req, res);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+        expect(spy).toHaveBeenCalledWith(405);
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+
+    it('should respond with one available method', () => {
+
+        const spy = jest.spyOn(res, 'send');
+
+        handleInvalidHttpMethod(['GET'])(req, res);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        expect(spy).toHaveBeenCalledWith({
+            message: 'method not allowed',
+            allowedMethods: 'GET'
+        });
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+
+    it('should respond with all available methods', () => {
+
+        const spy = jest.spyOn(res, 'send');
+
+        handleInvalidHttpMethod(['GET', 'PUT', 'POST'])(req, res);
+
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        expect(spy).toHaveBeenCalledWith({
+            message: 'method not allowed',
+            allowedMethods: 'GET, PUT, POST'
+        });
+
+        spy.mockReset();
+        spy.mockRestore();
+    });
+});

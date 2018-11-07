6bce595d63ef77e24b54c493bb2550aefe0d6f51
b-galazka <bartlomiejgalazka@zoho.eu>
2018-11-07 21:45:36 +0100

    remove todo

--- graphql/schema.js
+++ graphql/schema.js
@@ -6,7 +6,6 @@ const RootMutation = require('./mutations');
 // TODO: fix tests
 // TODO: add GraphQL tests
 // TODO: catch errors
-// TODO: use enmus where it's possible
 // TODO: add timetable update mutations
 module.exports = new GraphQLSchema({
     query: RootQuery,
--- graphql/types/mobileApp.js
+++ graphql/types/mobileApp.js
@@ -20,7 +20,6 @@ const TimetableType = new GraphQLObjectType({
     name: 'MobileAppTimetable',
     fields: () => ({
         _id: { type: GraphQLObjectId },
-        // TODO: use enum
         type: { type: GraphQLString },
         slug: { type: GraphQLString },
     })
@@ -29,7 +28,6 @@ const TimetableType = new GraphQLObjectType({
 const InputTimetableType = new GraphQLInputObjectType({
     name: 'InputMobileAppTimetable',
     fields: () => ({
-        // TODO: use enum
         type: { type: new GraphQLNonNull(GraphQLString) },
         slug: { type: new GraphQLNonNull(GraphQLString) },
     })

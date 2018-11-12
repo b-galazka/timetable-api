1ed1ff549b69d8c4629e240d1815673b936da439
b-galazka <bartlomiejgalazka@zoho.eu>
2018-11-12 14:01:51 +0100

    remove todo

--- graphql/schema.js
+++ graphql/schema.js
@@ -3,7 +3,6 @@ const { GraphQLSchema } = require('graphql');
 const RootQuery = require('./queries');
 const RootMutation = require('./mutations');
 
-// TODO: fix tests
 // TODO: add GraphQL tests
 module.exports = new GraphQLSchema({
     query: RootQuery,

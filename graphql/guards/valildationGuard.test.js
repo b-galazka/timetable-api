08f858771ceb914931e8e64171f34d032a24512c
b-galazka <bartlomiejgalazka@zoho.eu>
2018-11-24 17:15:02 +0100

    add graphql validation guard tests

---
+++ graphql/guards/valildationGuard.test.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+const validationGuard = require('./validationGuard');
+const ErrorResponse = require('../errors/ErrorResponse');
+
+describe('GraphQL validation guard', () => {
+
+    it('should return a function', () => {
+
+        expect(validationGuard()).toBeInstanceOf(Function);
+    });
+
+    it('should throw valid ErrorResponse if validation error has occured', () => {
+
+        const validationSchema = Joi.string();
+        const args = {};
+        const { message } = Joi.validate(args, validationSchema).error;
+        const resolver = validationGuard(validationSchema);
+
+        try {
+
+            resolver({}, args);
+
+        } catch (err) {
+
+            expect(err).toEqual(new ErrorResponse(message, 400));
+        }
+    });
+
+    it('should not throw any error if validation error has not occured', () => {
+
+        const validationSchema = Joi.object();
+        const resolver = validationGuard(validationSchema);
+
+        resolver({}, {});
+    });
+});

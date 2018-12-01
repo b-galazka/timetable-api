ed76482d473437336accb9df30703405a277583d
b-galazka <bartlomiejgalazka@zoho.eu>
2018-12-01 14:37:45 +0100

    add GraphQL timetable.findSingleSchoolClass query resolver tests

--- graphql/queries/resolvers/timetable.test.js
+++ graphql/queries/resolvers/timetable.test.js
@@ -14,7 +14,8 @@ const {
     findHours,
     findUpdateRequests,
     findSingleTeacher,
-    findSingleClassroom
+    findSingleClassroom,
+    findSingleSchoolClass
 } = require('./timetable');
 
 jest.mock('../../../utils/logger', () => require('../../../mocks/utils/logger'));
@@ -538,6 +539,130 @@ describe('GraphQL timetable.findSingleClassroom query resolver', () => {
         afterEach(() => {
 
             Classroom.findOne = originalFindOneMethod;
+        });
+    });
+});
+
+describe('GraphQL timetable.findSingleSchoolClass query resolver', () => {
+
+    let dbResponse;
+
+    describe('slug not provided', () => {
+
+        const originalFindOneMethod = SchoolClass.findOne;
+
+        beforeEach(() => {
+
+            SchoolClass.findOne = (criteria, fields, options) => {
+
+                const areCriteriaValid = isEqual(criteria, {});
+                const areFieldsValid = isEqual(fields, {});
+                const areOptionsValid = isEqual(options, { sort: { slug: 1 } });
+
+                if (areCriteriaValid && areFieldsValid && areOptionsValid) {
+
+                    return Promise.resolve(dbResponse);
+                }
+
+                return Promise.resolve('SchoolClass.findOne called with invalid params');
+            };
+        });
+
+        it('should return found record', async () => {
+
+            expect.assertions(2);
+
+            const values = [{ a: 10 }, { b: 20 }];
+
+            for (const value of values) {
+
+                dbResponse = value;
+
+                const result = await findSingleSchoolClass({}, {});
+
+                expect(result).toBe(value);
+            }
+        });
+
+        it('should throw valid ErrorResponse if error has occured during', async () => {
+
+            expect.assertions(1);
+
+            SchoolClass.findOne = () => Promise.reject(new Error('error message'));
+
+            try {
+
+                await findSingleSchoolClass({}, {});
+
+            } catch (err) {
+
+                expect(err).toEqual(new ErrorResponse('something went wrong', 500));
+            }
+        });
+
+        afterEach(() => {
+
+            SchoolClass.findOne = originalFindOneMethod;
+        });
+    });
+
+    describe('slug provided', () => {
+
+        const originalFindOneMethod = SchoolClass.findOne;
+        const slug = 'XYZ';
+
+        beforeEach(() => {
+
+            SchoolClass.findOne = (criteria, fields, options) => {
+
+                const areCriteriaValid = isEqual(criteria, { slug });
+                const areFieldsValid = isEqual(fields, {});
+                const areOptionsValid = isEqual(options, { sort: { slug: 1 } });
+
+                if (areCriteriaValid && areFieldsValid && areOptionsValid) {
+
+                    return Promise.resolve(dbResponse);
+                }
+
+                return Promise.resolve('SchoolClass.findOne called with invalid params');
+            };
+        });
+
+        it('should return found record', async () => {
+
+            expect.assertions(2);
+
+            const values = [{ a: 10 }, { b: 20 }];
+
+            for (const value of values) {
+
+                dbResponse = value;
+
+                const result = await findSingleSchoolClass({}, { slug });
+
+                expect(result).toBe(value);
+            }
+        });
+
+        it('should throw valid ErrorResponse if error has occured during', async () => {
+
+            expect.assertions(1);
+
+            SchoolClass.findOne = () => Promise.reject(new Error('error message'));
+
+            try {
+
+                await findSingleSchoolClass({}, { slug });
+
+            } catch (err) {
+
+                expect(err).toEqual(new ErrorResponse('something went wrong', 500));
+            }
+        });
+
+        afterEach(() => {
+
+            SchoolClass.findOne = originalFindOneMethod;
         });
     });
 });
--- graphql/schema.js
+++ graphql/schema.js
@@ -3,7 +3,6 @@ const { GraphQLSchema } = require('graphql');
 const RootQuery = require('./queries');
 const RootMutation = require('./mutations');
 
-// TODO: add GraphQL tests
 module.exports = new GraphQLSchema({
     query: RootQuery,
     mutation: RootMutation
